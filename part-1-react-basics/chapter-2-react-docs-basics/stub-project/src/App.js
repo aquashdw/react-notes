4bff9f8d0d66ac492b9c11449eb0e07d45e94276
aquashdw <aquashdw@gmail.com>
2022-05-25 18:06:20 +0900

    class component and functional component

--- part-1-react-basics/chapter-2-react-docs-basics/stub-project/src/App.js
+++ part-1-react-basics/chapter-2-react-docs-basics/stub-project/src/App.js
@@ -2,10 +2,14 @@ import './App.css';
 import Composition from "./components/4-props/Composition";
 import BeforeComment from "./components/4-props/Extraction/BeforeComment";
 import AfterComment from "./components/4-props/Extraction/AfterComment";
+import ClassComponent from "./components/5-state/ClassComponent";
+import FunctionalComponent from "./components/5-state/FunctionalComponent";
 
 function App() {
     return (
         <div className="App">
+            <ClassComponent />
+            <FunctionalComponent />
             <Composition />
             <BeforeComment />
             <AfterComment />
